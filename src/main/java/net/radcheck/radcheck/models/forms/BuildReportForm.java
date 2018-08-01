d5dbb2a90af67a422cfccdccbea5510bda6d17fa
bluenote87 <lurchworld@gmail.com>
2018-07-31 19:00:26 -0500

    Made images accessible to anyone, created a basic report form.

---
+++ src/main/java/net/radcheck/radcheck/models/forms/BuildReportForm.java
@@ -0,0 +1,11 @@
+package net.radcheck.radcheck.models.forms;
+
+import net.radcheck.radcheck.models.LatLon;
+import net.radcheck.radcheck.models.User;
+
+public class BuildReportForm {
+
+    private User user;
+
+    private Iterable<LatLon> locations;
+}
