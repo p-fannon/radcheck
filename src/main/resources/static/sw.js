c06ab6189cf9a40e098575e4e94d3dd2b9f64d0a
bluenote87 <lurchworld@gmail.com>
2018-10-08 10:56:02 -0500

    Fixed service worker to ignore POST requests.

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -66,23 +66,27 @@ self.addEventListener('activate', event => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+	if(event.request.method === "GET") {
+		// Skip cross-origin requests, like those for Google Analytics.
+		if (event.request.url.startsWith(self.location.origin)) {
+			event.respondWith(
+				caches.match(event.request).then(cachedResponse => {
+					if (cachedResponse) {
+						return cachedResponse;
+					}
+				return caches.open(RUNTIME).then(cache => {
+					return fetch(event.request).then(response => {
+					// Put a copy of the response in the runtime cache.
+						return cache.put(event.request, response.clone()).then(() => {
+							return response;
+						});
+					});
+				});
+				})
+			);
+		}
+  
+  } else {
+	return undefined;
   }
 });
