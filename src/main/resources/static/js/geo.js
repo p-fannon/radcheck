c2a1429a2f88240ccd192bc3174e927631a3e2ce
bluenote87 <lurchworld@gmail.com>
2018-11-23 16:20:03 -0600

    Refactored the search view for ease of use

--- src/main/resources/static/js/geo.js
+++ src/main/resources/static/js/geo.js
@@ -1,13 +1,29 @@
 document.getElementById('quickSearch').addEventListener('click', function() {
-    console.log("Prompt fired");
+    console.log("Geolocation prompt fired");
     if ("geolocation" in navigator) {
         // check if geolocation is supported/enabled on current browser
         navigator.geolocation.getCurrentPosition(
             function success(position) {
              // for when getting location is a success
-                document.getElementById('latitude').value = position.coords.latitude;
-                document.getElementById('longitude').value = position.coords.longitude;
-                console.log('latitude', position.coords.latitude, 'longitude', position.coords.longitude);
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
+                document.getElementById('latitude').value = latitude;
+                document.getElementById('longitude').value = longitude;
+                console.log('latitude: ', latitude, ', longitude: ', longitude);
+                var searchResult = new google.maps.LatLng(latitude, longitude);
+                var mapOptions = {
+                                 zoom: 17,
+                                 center: searchResult,
+                             }
+                var newMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                var marker = new google.maps.Marker({
+                                        map: newMap,
+                                        position: searchResult
+                                    });
+                document.getElementById('quickSearch').innerText = "Success! Please wait...";
+                document.getElementById('locate').disabled = true;
+                document.getElementById('quickSearch').disabled = true;
                 document.getElementById("mapForm").submit();
              },
             function error(error_message) {
